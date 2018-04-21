744effd12f8ec745adb1bc75aedc0dac3af52e8f
Michael Sober <michael.sober@ymail.com>
2018-04-21 15:53:01 +0200

    Added dummy admin data for testing

--- frontend/src/app/readable-dmp/readable-dmp.component.ts
+++ frontend/src/app/readable-dmp/readable-dmp.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {AdministrativeData} from "../model/administrative-data";
 
 @Component({
   selector: 'app-readable-dmp',
@@ -8,11 +9,19 @@ import { Component, OnInit } from '@angular/core';
 export class ReadableDmpComponent implements OnInit {
 
   date: Date;
+  administrativeData: AdministrativeData;
 
   constructor() { }
 
   ngOnInit() {
     this.date = new Date();
+    this.administrativeData = { orcid: "https://orcid.org/0000-0002-9612-9022",
+      family_name: "Sober",
+      given_name: "Michael",
+      email: "michael.sober@ymail.com",
+      country: "AT",
+      project_title: "Project X"
+    };
   }
 
 }
